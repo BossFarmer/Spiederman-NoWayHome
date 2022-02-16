d34a235b909737e42de0260e860b4aac790883fc
Khizer Siddiqui <Khizer.siddiqui@hotmail.com>
2022-02-16 16:09:56 +0100

    map finish with items

--- Assets/Scripts/BarrierScript.cs
+++ Assets/Scripts/BarrierScript.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class BarrierScript : MonoBehaviour
 {
-    public int killDeathCount;
-    public GameObject Barrier1;
-    public GameObject Barrier11;
+    public int KillDeathCount;
+    //public int killDeathCount2;
+    public GameObject Barrier1;//P1Barriere1;
+    public GameObject Barrier11;//P2Barriere1;
     public GameObject Barrier2;
     public GameObject Barrier22;
     public GameObject Barrier3;
     public GameObject Barrier33;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
        
     }
@@ -21,64 +22,64 @@ public class BarrierScript : MonoBehaviour
     private void Update()
     {
         SetBarrier();
+    }
 
-        void SetBarrier()
+    public void SetBarrier()
+    {
+        switch (KillDeathCount)
         {
-            switch (killDeathCount)
-            {
-                case 0:
-                    if (killDeathCount == 0)
-                    {
-                        Barrier1.SetActive(true);
-                        Barrier11.SetActive(true);
-                    }
-                    break;
-                case 1:
-                    if (killDeathCount == 1)
-                    {
-                        Barrier1.SetActive(false);
-                        Barrier2.SetActive(true);
-                    }
-                    break;
-                case 2:
-                    if (killDeathCount == 2)
-                    {
-                        Barrier2.SetActive(false);
-                        Barrier3.SetActive(true);
-                        Barrier1.SetActive(true);
-                    }
-                    break;
-                case 3:
-                    if (killDeathCount == 3)
-                    {
-                        Barrier3.SetActive(false);
-                        Barrier2.SetActive(true);
-                    }
-                    break;
-                case 4:
-                    if (killDeathCount == -1)
-                    {
-                        Barrier11.SetActive(false);
-                        Barrier22.SetActive(true);
-                    }
-                    break;
-                case 5:
-                    if (killDeathCount == -2)
-                    {
-                        Barrier22.SetActive(false);
-                        Barrier33.SetActive(true);
-                    }
-                    break;
-                case 6:
-                    if (killDeathCount == -2)
-                    {
-                        Barrier33.SetActive(false);
-                    }
-                    break;
+            case 0:
+                if (KillDeathCount == 0)
+                {
+                    Barrier1.SetActive(true);
+                    Barrier11.SetActive(true);
+                }
+                break;
+            case 1:
+                if (KillDeathCount == 1)
+                {
+                    Barrier1.SetActive(false);
+                    Barrier2.SetActive(true);
+                }
+                break;
+            case 2:
+                if (KillDeathCount == 2)
+                {
+                    Barrier2.SetActive(false);
+                    Barrier3.SetActive(true);
+                    Barrier1.SetActive(true);
+                }
+                break;
+            case 3:
+                if (KillDeathCount == 3)
+                {
+                    Barrier3.SetActive(false);
+                    Barrier2.SetActive(true);
+                }
+                break;
+            case -1:
+                if (KillDeathCount == -1)
+                {
+                    Barrier11.SetActive(false);
+                    Barrier22.SetActive(true);
+                }
+                break;
+            case -2:
+                if (KillDeathCount == -2)
+                {
+                    Barrier22.SetActive(false);
+                    Barrier33.SetActive(true);
+                }
+                break;
+            case -3:
+                if (KillDeathCount == -3)
+                {
+                    Barrier33.SetActive(false);
+                }
+                break;
 
-                default:
-                    break;
-            }
+            default:
+                break;
         }
     }
 }
