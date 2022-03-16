4988408ce1037b08ba9d294fcf5a210c8d5996c2
Khizer Siddiqui <Khizer.siddiqui@hotmail.com>
2022-03-17 00:55:15 +0100

    GoodUpdateBarrier

---
+++ Assets/PlayerOneScript.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneScript : MonoBehaviour
+{
+    private int healthP1 = 150;
+    public int currentHealthP1;
+    private int deathCountPlayer1Spawn;
+    private int deathCountPlayer1Barrierr;
+    private PlayerTwoScript plTwoScript;
+    // Start is called before the first frame update
+
+    public event Action<int> OnPlayerOneDeathBarrier;
+    public event Action<int, GameObject > OnPlayer1DeathSpawn;
+    public event Action OnPlayerOneKilled;
+
+    void Awake()
+    {
+        plTwoScript = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerTwoScript>();
+        plTwoScript.OnPlayer2Killed += PlayerTwoKilled;
+    }
+
+    private void PlayerTwoKilled()
+    {
+        deathCountPlayer1Spawn--;
+        deathCountPlayer1Barrierr--;
+        Debug.Log("spawncount player one: "+ deathCountPlayer1Spawn);
+    }
+
+    void Start()
+    {
+        ResetHealth();
+        deathCountPlayer1Barrierr = 0;
+        deathCountPlayer1Spawn = 0;
+    }
+
+    void Update()
+    {
+        IsPlayerDead();
+    }
+
+    public void TakeDamagePlayer(int Damage)
+    {
+        currentHealthP1 -= Damage;
+        Debug.Log("Player One health: " + currentHealthP1);
+    }
+
+    public void IsPlayerDead()
+    {
+        if (currentHealthP1 <= 0)
+        {
+            Debug.Log("Player One was killed!");
+            Debug.Log("PlayerOne Spawnpoint: " + deathCountPlayer1Barrierr);
+            deathCountPlayer1Barrierr++;
+            deathCountPlayer1Spawn++;
+            Debug.Log("PlayerOne Spawnpoint: " + deathCountPlayer1Barrierr);
+            OnPlayerOneDeathBarrier?.Invoke(deathCountPlayer1Barrierr);
+            OnPlayer1DeathSpawn?.Invoke(deathCountPlayer1Spawn, this.gameObject);
+            OnPlayerOneKilled?.Invoke();
+            ResetHealth();
+
+        }
+    }
+
+    public void ResetHealth()
+    {
+        currentHealthP1 = healthP1;
+    }
+}
