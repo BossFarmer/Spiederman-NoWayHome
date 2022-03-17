e40a017b72cd05338e408af935b415e5f22ec281
Per Langtimm <per.langtimm@icloud.com>
2022-03-17 05:05:45 +0100

    Inventory Finished

--- Assets/PlayerTwoScript.cs
+++ Assets/PlayerTwoScript.cs
@@ -11,10 +11,24 @@ public class PlayerTwoScript : MonoBehaviour
     private int deathCountPlayer2Spawn;
     private bool temp = true;
     private PlayerOneScript plOneScript;
-
+    public WeaponPickupScript weaponPickupScript;
     public event Action<int> OnPlayer2DeathBarrier;
     public event Action<int, GameObject> OnPlayer2DeathSpawn;
     public event Action OnPlayer2Killed;
+    public int sizeOfList;
+    public List<GameObject> Inventar;
+    public GameObject PrimaryWeapon;
+    public GameObject SekundaryWeapon;
+
+    #region Waffenliste
+    public GameObject Pistol;
+    public GameObject Ak47;
+    public GameObject Autopump;
+    public GameObject Deagle;
+    public GameObject M16;
+    public GameObject Pump;
+    public GameObject Sniper;
+    #endregion
 
     void Awake()
     {
@@ -27,6 +41,8 @@ public class PlayerTwoScript : MonoBehaviour
         deathCountPlayer2Spawn--;
         deathCountPlayer2Barrier--;
         Debug.Log("spawncount player2 : " + deathCountPlayer2Spawn);
+        Inventar = new List<GameObject>(1);
+
     }
 
     private void OnPlayerOneKilled()
@@ -68,8 +84,92 @@ public class PlayerTwoScript : MonoBehaviour
         }
     }
 
+    
     public void ResetHealth()
     {
         currentHealthP2 = healthP2;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapons")
+        {
+            weaponPickupScript = other.GetComponent<WeaponPickupScript>();
+            switch (weaponPickupScript.WeaponType)
+            {
+                case WeaponPickupScript.EWeapons.Ak47:
+                    if(Inventar.Count == 0)
+                    {
+                        PrimaryWeapon = Ak47;
+                    }
+                    else
+                    {
+                        SekundaryWeapon = Ak47;
+                    }
+                    break;
+                case WeaponPickupScript.EWeapons.AutoPump:
+                    if (Inventar.Count == 0)
+                    {
+                        PrimaryWeapon = Autopump;
+                    }
+                    else
+                    {
+                        SekundaryWeapon = Autopump;
+                    }
+                    break;
+                case WeaponPickupScript.EWeapons.Deagle:
+                    if (Inventar.Count == 0)
+                    {
+                        PrimaryWeapon = Deagle;
+                    }
+                    else
+                    {
+                        SekundaryWeapon = Deagle;
+                    }
+                    break;
+                case WeaponPickupScript.EWeapons.M16:
+                    if (Inventar.Count == 0)
+                    {
+                        PrimaryWeapon = M16;
+                    }
+                    else
+                    {
+                        SekundaryWeapon = M16;
+                    }
+                    break;
+                case WeaponPickupScript.EWeapons.Pistol:
+                    if (Inventar.Count == 0)
+                    {
+                        PrimaryWeapon = Pistol;
+                    }
+                    else
+                    {
+                        SekundaryWeapon = Pistol;
+                    }
+                    break;
+                case WeaponPickupScript.EWeapons.Pump:
+                    if (Inventar.Count == 0)
+                    {
+                        PrimaryWeapon = Pump;
+                    }
+                    else
+                    {
+                        SekundaryWeapon = Pump;
+                    }
+                    break;
+                case WeaponPickupScript.EWeapons.Sniper:
+                    if (Inventar.Count == 0)
+                    {
+                        PrimaryWeapon = Sniper;
+                    }
+                    else
+                    {
+                        SekundaryWeapon = Sniper;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
     }
 }
--- Assets/Scripts/KhizerScripts/EquipmentScript.cs
+++ Assets/Scripts/KhizerScripts/EquipmentScript.cs
@@ -9,6 +9,13 @@ public class EquipmentScript : MonoBehaviour
     private int nextWeapon;
     private float mouseInput;
     private float controllerInput = 0f;
+    [SerializeField]
+    private PlayerOneScript Pl1Script;
+    [SerializeField]
+    private PlayerTwoScript Pl2Script;
+    private ConnectWeapon connectWeapon1, connectWeapon2;
+    private int magSize1, magSize2, magSize3, magSize4;
+    
 
     private void Awake()
     {
@@ -50,6 +57,8 @@ public class EquipmentScript : MonoBehaviour
     private void Update()
     {
         ChangeWeapon();
+        CheckWeapon();
+        Debug.Log("Magazin Größe"+ magSize1);
     }
 
     void ChoseWeapon()
@@ -68,6 +77,27 @@ public class EquipmentScript : MonoBehaviour
             index++;
         }
     }
+
+    private void CheckWeapon()
+    {
+        //if (magSize1 >= 0)
+        //{
+        //    Pl1Script.Inventar.RemoveAt(1);
+        //}
+        //if (magSize2 >= 0)
+        //{
+        //    Pl2Script.Inventar.RemoveAt(1);
+        //}
+        //if (magSize3 >= 0)
+        //{
+        //    Pl1Script.Inventar.RemoveAt(2);
+        //}
+        //if (magSize4 >= 0)
+        //{
+        //    Pl2Script.Inventar.RemoveAt(2);
+        //}
+    }
+
     private void CheckPlayer()
     {
         PlayerInputActions action = new PlayerInputActions();
@@ -76,6 +106,7 @@ public class EquipmentScript : MonoBehaviour
             //Debug.Log("this Player one");
             action.Player.Enable();
             action.Player.ChangeWeaponsMouse.performed += MouseInputScroll;
+            Pl1Script = transform.root.gameObject.GetComponent<PlayerOneScript>();
         }
 
         if (transform.parent.tag == "SecondCamera")
@@ -84,7 +115,20 @@ public class EquipmentScript : MonoBehaviour
             action.Player2.Enable();
             action.Player2.ChangeWeaponsControllerRight.performed += ControllerInput;
             action.Player2.ChangeWeaponsControllerLeft.performed += ControllerInputLeft;
+            Pl2Script = transform.root.gameObject.GetComponent<PlayerTwoScript>();
         }
+    }
+    public void WaffenZuweisung()
+    {
+        connectWeapon1 = Pl1Script.PrimaryWeapon.gameObject.GetComponent<ConnectWeapon>();
+        connectWeapon2 = Pl2Script.PrimaryWeapon.gameObject.GetComponent<ConnectWeapon>();
+        connectWeapon1 = Pl1Script.SekundaryWeapon.gameObject.GetComponent<ConnectWeapon>();
+        connectWeapon2 = Pl2Script.SekundaryWeapon.gameObject.GetComponent<ConnectWeapon>();
+        magSize1 = connectWeapon1.WMagazineSize;
+        magSize2 = connectWeapon2.WMagazineSize;
+        magSize3 = connectWeapon1.WMagazineSize;
+        magSize4 = connectWeapon2.WMagazineSize;
+
     }
 
     void ChangeWeapon()
@@ -92,29 +136,40 @@ public class EquipmentScript : MonoBehaviour
         nextWeapon = currentWeapon;
         if (mouseInput > 0 || controllerInput > 0)
         {
-            controllerInput = 0;
-            if (currentWeapon >= transform.childCount -1)
-            {
-                currentWeapon = 0;
-            }
-            else
-            {
-                currentWeapon++;
-            }
+            //controllerInput = 0;
+            //if (currentWeapon >= transform.childCount - 1)
+            //{
+            //    currentWeapon = 0;
+            //}
+            //else
+            //{
+            //    currentWeapon++;
+            //}
+            
+            
+            Pl1Script.PrimaryWeapon.SetActive(true);
+            Pl1Script.SekundaryWeapon.SetActive(false);
+            Pl2Script.PrimaryWeapon.SetActive(true);
+            Pl2Script.SekundaryWeapon.SetActive(false);
         }
         if (mouseInput < 0 || controllerInput < 0)
         {
-            controllerInput = 0;
-            if (currentWeapon <= 0 )
-            {
-                currentWeapon = transform.childCount - 1;
-            }
-            else
-            {
-                currentWeapon--;
-            }
+            //controllerInput = 0;
+            //if (currentWeapon <= 0)
+            //{
+            //    currentWeapon = transform.childCount - 1;
+            //}
+            //else
+            //{
+            //    currentWeapon--;
+            //}
+            
+            Pl1Script.PrimaryWeapon.SetActive(false);
+            Pl1Script.SekundaryWeapon.SetActive(true);
+            Pl2Script.PrimaryWeapon.SetActive(false);
+            Pl2Script.SekundaryWeapon.SetActive(true);
         }
-        if (nextWeapon!= currentWeapon)
+        if (nextWeapon != currentWeapon)
         {
             ChoseWeapon();
         }
